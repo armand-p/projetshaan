ff4a862f6d9836a4aa33dd2257a47d4b179807a9
Sylvain <sylh43@hotmail.fr>
2019-12-04 16:08:08 +0100

    service app-config

---
+++ shaan-angular/src/app/personnage/personnage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personnage',
+  templateUrl: './personnage.component.html',
+  styleUrls: ['./personnage.component.css']
+})
+export class PersonnageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
