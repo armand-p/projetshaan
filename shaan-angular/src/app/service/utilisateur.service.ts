fa3e97cd9a2f16ceb7a301584d3dee1f87755f03
theau <tpoixpro@gmail.com>
2019-12-05 13:50:01 +0100

    user service

--- shaan-angular/src/app/service/utilisateur.service.ts
+++ shaan-angular/src/app/service/utilisateur.service.ts
@@ -8,34 +8,61 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class UtilisateurService {
-
-  private utilisateurs: any;
+  private maitreDuJeus : any;
+  private joueurs: any;
 
   constructor(private http: HttpClient, private appConfigService: AppConfigService) {
     this.load();
   }
 
   load() {
-    this.http.get(this.appConfigService.backEnd + 'utilisateur')
-      .subscribe(resp => this.utilisateurs = resp);
+    this.http.get(this.appConfigService.backEnd + 'joueur')
+      .subscribe(resp => this.joueurs = resp);
+    this.http.get(this.appConfigService.backEnd + 'mj')
+      .subscribe(resp => this.maitreDuJeus = resp);
   }
 
-  public findAll(): Array<Utilisateur> {
-    return this.utilisateurs;
-  }
-
-  findById(id: number): Observable<any> {
-    return this.http.get(this.appConfigService.backEnd + 'utilisateur/' + id);
-  }
-
-  save(utilisateur: Utilisateur) {
-    if (utilisateur.id) {
-      this.http.put(this.appConfigService.backEnd + 'utilisateur/' + utilisateur.id, utilisateur).subscribe(resp => this.load());
-    } else {
-      this.http.post(this.appConfigService.backEnd + 'utilisateur/', utilisateur).subscribe(resp => this.load());
+  public findAll(type:string): Array<Utilisateur> {
+    if(type === "joueur"){
+    return this.joueurs;}
+    else if(type=== "maitreDuJeu"){
+      return this.maitreDuJeus
     }
   }
 
-  deleteBydId(id: number) {
-    this.http.delete(this.appConfigService.backEnd + 'utilisateur/' + id).subscribe(resp => this.load());
+  findById(id: number,type:string): Observable<any> {
+    if(type === "joueur"){
+      return this.http.get(this.appConfigService.backEnd + 'joueur/' + id);}
+    else if(type=== "maitreDuJeu"){
+      return this.http.get(this.appConfigService.backEnd + 'mj/' + id);
+    }
+
+  }
+
+  save(utilisateur: Utilisateur) {
+    if(utilisateur.type="maitreDuJeu"){
+      if (utilisateur.id) {
+        this.http.put(this.appConfigService.backEnd + 'mj/' + utilisateur.id, utilisateur).subscribe(resp => this.load());
+      } else {
+        this.http.post(this.appConfigService.backEnd + 'mj/', utilisateur).subscribe(resp => this.load());
+      }
+
+    }
+    else{
+      if (utilisateur.id) {
+        this.http.put(this.appConfigService.backEnd + 'joueur/' + utilisateur.id, utilisateur).subscribe(resp => this.load());
+      } else {
+        this.http.post(this.appConfigService.backEnd + 'joueur/', utilisateur).subscribe(resp => this.load());
+      }
+
+    }
+  }
+
+  deleteBydId(id: number,type:string) {
+    if(type === "joueur"){
+      this.http.delete(this.appConfigService.backEnd + 'joueur/' + id).subscribe(resp => this.load());}
+    else if(type=== "maitreDuJeu"){
+      this.http.delete(this.appConfigService.backEnd + 'mj/' + id).subscribe(resp => this.load());
+    }
+
   }}
