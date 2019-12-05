8957d822f2e496459ecb398d4e34e668ca8fc18c
Baptiste <baptiste.devaux.1993@gmail.com>
2019-12-05 11:19:46 +0100

    b

--- shaan-angular/src/app/model/TableDeJeu.ts
+++ shaan-angular/src/app/model/TableDeJeu.ts
@@ -10,14 +10,14 @@ export class TableDeJeu {
   maitreDuJeu : MaitreDuJeu;
 
 
-  constructor(id?: number, version?: number, nom?: string, notes?: string, statut?: boolean, connection?: boolean, maitreDuJeu?:MaitreDuJeu) {
+  constructor(id?: number, version?: number, nom?: string, notes?: string, statut?: boolean, connection?: boolean, maitreDuJeu? : MaitreDuJeu) {
     this.id = id;
     this.version = version;
     this.nom = nom;
     this.notes = notes;
     this.statut = statut;
     this.connection = connection;
-    this.maitreDuJeu= maitreDuJeu;
+    this.maitreDuJeu = maitreDuJeu;
   }
 
 }
