73074924a089e03be7d644c29267cbc7a6a6cd14
Baptiste <baptiste.devaux.1993@gmail.com>
2019-12-05 13:05:21 +0100

    service domaine/motivation/pouvoir/tabledejeu

---
+++ shaan-angular/src/app/service/domaine.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AppConfigService} from '../app-config.service';
+import {Domaine} from '../model/domaine';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class domaineService {
+
+  private domaines: any;
+
+  constructor(private http: HttpClient, private appConfigService: AppConfigService) {
+    this.load();
+  }
+
+  load() {
+    this.http.get(this.appConfigService.backEnd + 'domaine')
+      .subscribe(resp => this.domaines = resp);
+  }
+
+  public findAll(): Array<Domaine> {
+    return this.domaines;
+  }
+
+  findById(id: number): Observable<any> {
+    return this.http.get(this.appConfigService.backEnd + 'domaine/' + id);
+  }
+
+  save(domaine: Domaine) {
+    if (domaine.id) {
+      this.http.put(this.appConfigService.backEnd + 'domaine/' + domaine.id, domaine).subscribe(resp => this.load());
+    } else {
+      this.http.post(this.appConfigService.backEnd + 'domaine/', domaine).subscribe(resp => this.load());
+    }
+  }
+
+  deleteBydId(id: number) {
+    this.http.delete(this.appConfigService.backEnd + 'domaine/' + id).subscribe(resp => this.load());
+  }}
