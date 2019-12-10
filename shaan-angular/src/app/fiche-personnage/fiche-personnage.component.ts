151525736be028a67796ba64ca18c2b6cc224cd9
armand <armandpeyrelongue@gmail.com>
2019-12-10 17:26:39 +0100

    fiche perso

--- shaan-angular/src/app/fiche-personnage/fiche-personnage.component.ts
+++ shaan-angular/src/app/fiche-personnage/fiche-personnage.component.ts
@@ -5,6 +5,8 @@ import {ActivatedRoute} from "@angular/router";
 import {DomainePersonnageService} from "../service/domaine-personnage.service";
 import {DomainePersonnage} from "../model/DomainePersonnage";
 import {TableDeJeu} from "../model/TableDeJeu";
+import {Specialisation} from "../model/Specialisation";
+import {SpecialisationService} from "../service/specialisation.service";
 
 @Component({
   selector: 'app-fiche-personnage',
@@ -17,13 +19,14 @@ export class FichePersonnageComponent implements OnInit {
   id: number;
   private Domaines: Array<DomainePersonnage>;
   listDomaines: Array<DomainePersonnage>;
+  listSpecialisations: Array<Specialisation>;
 
-  constructor(private domainePersonnageService: DomainePersonnageService, private personnageService: PersonnageService, private route: ActivatedRoute) {
+  constructor(private specialisationService:SpecialisationService, private domainePersonnageService: DomainePersonnageService, private personnageService: PersonnageService, private route: ActivatedRoute) {
     this.route.params.subscribe(params => this.id = params.id);
     this.personnageService.findById(this.id).subscribe(resp => this.personnage = resp);
-    this.personnageService.domaineperso(this.id).subscribe(resp => {
-      this.listDomaines = resp;
-    })
+    this.personnageService.domaineperso(this.id).subscribe(resp => {this.listDomaines = resp})
+    this.specialisationService.speDomaines(this.id).subscribe(resp => this.listSpecialisations = resp);
+
   }
 
   ngOnInit() {
